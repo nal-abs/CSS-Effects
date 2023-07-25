3b027398a4161133b8d26203e41e6f5c9102bd63
nal-abs <nal@abstractit.in>
2023-07-25 12:01:23 +0530

    Some improvements done in mask component.

--- src/stories/Mask.stories.js
+++ src/stories/Mask.stories.js
@@ -4,6 +4,22 @@ import CssMask from './css/Mask.js';
 const component = {
 	title: 'CSS/Mask',
 	component: CssMask,
+	argTypes: {
+		URL: { control: 'radio', if: { arg: 'imageType', eq: 'url' },
+			options: ['airplane.png',
+				'animate.svg',
+				'animatedWolf.png',
+				'semiTransparent.png'] },
+		LinearGradient: { control: 'radio',
+			if: { arg: 'imageType', eq: 'linear-gradient' },
+			options: ['linear-gradient'] },
+		imageType: {
+			type: 'select',	options: ['url', 'linear-gradient'],
+		},
+	},
+	args: {
+		imageType: 'url',
+	},
 };
 
 export default component;
--- src/stories/css/Mask.js
+++ src/stories/css/Mask.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import svgImage from '../../images/SvgImage.svg';
+import pngImage from '../../images/fighterJet.png';
+import wolfImage from '../../images/wolf.png';
+import Butterfly from '../../images/Butterfly.png';
 
-const Mask = () =>
-	<div className="mask">
+const url = {
+	'airplane.png': pngImage,
+	'animate.svg': svgImage,
+	'animatedWolf.png': wolfImage,
+	'semiTransparent.png': Butterfly,
+};
+
+const gradient = {
+	'linear-gradient': 'to bottom, transparent 25%, black 75%',
+};
+
+const Mask = ({ URL, imageType, LinearGradient }) => {
+	const type = URL || LinearGradient;
+	const img = URL ? url : gradient;
+
+	return <div className="mask" style={ { WebkitMaskImage: `${ imageType }(${ img[type] })` } }>
 		<div className="backgroundMask"/>
 	</div>;
+};
 
 export default Mask;
