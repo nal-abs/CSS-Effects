21ccc85a85cbd8729f124ce6ffcc2bada5a32f60
nal-abs <nal@abstractit.in>
2023-08-01 09:16:45 +0530

    Introduced some more effects in transition.

--- src/stories/css/BookTransition.js
+++ src/stories/css/BookTransition.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable indent */
 import { range } from '@laufire/utils/collection';
 import React from 'react';
 import { Box } from '@mui/material';
@@ -6,12 +8,17 @@ const bookCount = 5;
 const inclination = 20;
 
 const BookEffect = () =>
-	<Box className="book-parent">{range(1, bookCount).map((dummy, index) =>
-		<Box
-			key={ index }
-			sx={ { 'transform': `rotate(${ (dummy + 1) * inclination }deg) scale(0.5, 0.5)`,
-				'&:hover': { transform: 'rotate(0deg) scale(1,1)' }} }
-			className="book"
-		>Hello! welcome</Box>)}</Box>;
+	<div>
+		<Box className="book-parent">{range(1, bookCount).map((dummy, index) =>
+			<Box
+				key={ index }
+				sx={ { 'transform': `rotate(${ (dummy + 1) * inclination }deg) scale(0.5, 0.5)`,
+					'&:hover': { transform: 'rotate(0deg) scale(1,1)' }} }
+				className="book"
+			>Hello!<Box
+				className="welcome"
+				sx={ { color: 'black' } }
+			       >welcome</Box></Box>)}</Box>
+	</div>;
 
 export default BookEffect;
