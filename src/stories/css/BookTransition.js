43c33ad4f93237f8a22c8d753bbfea56ddb0e7ea
nal-abs <nal@abstractit.in>
2023-08-01 18:54:22 +0530

    Refactored.

--- src/stories/css/BookTransition.js
+++ src/stories/css/BookTransition.js
@@ -8,17 +8,15 @@ const bookCount = 5;
 const inclination = 20;
 
 const BookEffect = () =>
-	<div>
-		<Box className="book-parent">{range(1, bookCount).map((dummy, index) =>
+	<Box className="book-parent">{range(1, bookCount).map((dummy, index) =>
+		<Box key={ index } className="parent">
 			<Box
-				key={ index }
 				sx={ { 'transform': `rotate(${ (dummy + 1) * inclination }deg) scale(0.5, 0.5)`,
 					'&:hover': { transform: 'rotate(0deg) scale(1,1)' }} }
 				className="book"
 			>Hello!<Box
-				className="welcome"
+				className="welcome fancy"
 				sx={ { color: 'black' } }
-			       >welcome</Box></Box>)}</Box>
-	</div>;
+			       >welcome</Box></Box></Box>)}</Box>;
 
 export default BookEffect;
